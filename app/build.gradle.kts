417ea64f6aa32260b1714ee3053d9f9e09f13a8f
MidKnight <patohacker458@gmail.com>
2026-06-05 14:36:55 -0600

    build: agrega window-size-class y estructura de carpetas base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("androidx.compose.material3:material3-window-size-class")
 }
