f2d5f19bd5c4f1e1f69c9c21014eb535470161f4
MidKnight <patohacker458@gmail.com>
2026-06-05 18:02:27 -0600

    chore: bump version to 0.2.0-alpha for public release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.midknight.pixelnotes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
