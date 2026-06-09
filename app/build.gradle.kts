5d924c5df02eab7c9ce62db4865d1cbaf3e76914
MidKnight <patohacker458@gmail.com>
2026-06-09 14:42:41 -0600

    chore: prepare v1.1.0-beta release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.midknight.pixelnotes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5.0-alpha"
+        versionCode = 6
+        versionName = "1.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
